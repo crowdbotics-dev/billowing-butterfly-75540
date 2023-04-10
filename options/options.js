2623d3149707a82b8d83d328eeb2c382e496d7e2
Crowdbotics <engineering@crowdbotics.com>
2023-04-10 00:16:00 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "billowing-butterfly-75540",
+  url: "https://billowing-butterfly-75540.botics.co",
+  api: "https://billowing-butterfly-75540.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
